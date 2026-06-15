ca60874278521ec83f561d671cc5f08fde85a3dc
Rakibul Islam <it24029@mbstu.ac.bd>
2026-06-15 10:17:44 +0600

    Shape , Circle and Rectangle Class uploaded

---
+++ src/shapes/Shape.java
@@ -0,0 +1,33 @@
+package shapes;
+
+public abstract class Shape {
+    private String color;
+    private boolean filled;
+    public Shape(String color, boolean filled){
+        this.color=color;
+        this.filled=filled;
+    }
+    public abstract double getArea();
+    public abstract  double getPerimeter();
+
+    @Override
+    public String toString(){
+        return "Color : " + color + "\n" +"Filled: " + (filled ? "Yes" : "No");
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public boolean getFilled(){
+        return filled;
+    }
+
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setFilled(boolean filled){
+        this.filled=filled;
+    }
+
+
+}
